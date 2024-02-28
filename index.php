99ee6ad6dc5736e77e5b830ed099af037cc1c486
tebero <tebero@taltech.ee>
2024-02-28 16:17:17 +0000

    Upload index

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+    <link href="./css/index-style.css" rel="stylesheet" />
+    <title>Finanss</title>
+</head>
+<body>
+<div class="container">
+    <div class="header"></div>
+
+    <!-- Updated position and style for brand and login -->
+    <span class="brand">Finanss</span>
+    <span class="login">Log in to your account</span>
+
+    <!-- Updated font size and position for description -->
+    <span class="description">Take control of your finances with ease, achieve your financial goals.</span>
+
+    <!-- Updated CTA section -->
+    <div class="cta-section">
+        <a href="#" class='cta-link'><span class='cta'>Ready to get started?</span></a>
+        <!-- Removed additional divs as they are not visible in the image -->
+    </div>
+</div>
+</body>
+</html>
