9e9481b6da5a5636257c24cbc0239b1abffd7acb
tebero <tebero@taltech.ee>
2024-02-28 16:49:56 +0000

    Add new file

---
+++ registration.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+    <link href="./css/registration-style.css" rel="stylesheet" />
+    <title>Registration</title>
+</head>
+<body>
+<div class="header"></div>
+<div class="user-info"></div>
+<span class="app-title">Finanss</span>
+
+<!-- Registration Form -->
+<div class="registration-form">
+    <h2>Registration</h2>
+    <form>
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+
+        <button type="submit">Register</button>
+    </form>
+</div>
+
+</body>
+</html>
