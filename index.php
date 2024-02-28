317f286e488a4ceabdd14542dd25da0b29b544ed
tebero <tebero@taltech.ee>
2024-02-28 16:20:09 +0000

    Update index.php

--- index.php
+++ index.php
@@ -11,17 +11,17 @@
 <div class="container">
     <div class="header"></div>
 
-    <!-- Updated position and style for brand and login -->
+    
     <span class="brand">Finanss</span>
     <span class="login">Log in to your account</span>
 
-    <!-- Updated font size and position for description -->
+    
     <span class="description">Take control of your finances with ease, achieve your financial goals.</span>
 
-    <!-- Updated CTA section -->
+    
     <div class="cta-section">
         <a href="#" class='cta-link'><span class='cta'>Ready to get started?</span></a>
-        <!-- Removed additional divs as they are not visible in the image -->
+        
     </div>
 </div>
 </body>
