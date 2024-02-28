f6f8df0d06858d077143689f43f5b7454a771a42
tebero <tebero@taltech.ee>
2024-02-28 16:52:20 +0000

    Add new file

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+    <link href="./css/login-style.css" rel="stylesheet" />
+    <title>Login</title>
+</head>
+<body>
+<div class="header"></div>
+<div class="user-info"></div>
+<span class="app-title">Finanss</span>
+
+<!-- Login Form -->
+<div class="login-form">
+    <h2>Login</h2>
+    <form>
+        <label for="username">Username or Email:</label>
+        <input type="text" id="username" name="username" required>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+
+        <label for="remember">Remember Me:</label>
+        <input type="checkbox" id="remember" name="remember">
+
+        <a href="password-reset.php" class="forgot-password">Forgot Password?</a>
+
+        <button type="submit">Login</button>
+    </form>
+</div>
+
+</body>
+</html>
