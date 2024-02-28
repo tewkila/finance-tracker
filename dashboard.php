2f395006a6c51e87809eee2d758f15bb23733d55
tebero <tebero@taltech.ee>
2024-02-28 16:22:07 +0000

    Upload New File

---
+++ dashboard.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+    <link href="./css/dashboard-style.css" rel="stylesheet" />
+    <title>Finance Dashboard</title>
+</head>
+<body>
+<div class="finance-dashboard">
+    <div class="header"></div>
+    <div class="user-info"></div>
+    <span class="app-title">Finance</span>
+    <div class="menu">
+        <span class="menu-item">Dashboard</span>
+        <span class="menu-item">Income</span>
+        <span class="menu-item">Expense</span>
+        <span class="menu-item">Budget</span>
+    </div>
+    <div class="footer"></div>
+    <span class="user">user</span>
+    <span class="welcome-message">Welcome back, user!</span>
+    <div class="balance-info">
+        <div class="icon"></div>
+        <span class="info-label">Balance</span>
+        <span class="balance-amount">12560$</span>
+        <span class="ellipsis">...</span>
+    </div>
+    <div class="expense-info">
+        <div class="icon"></div>
+        <span class="info-label">Expense</span>
+        <span class="expense-amount">-6784$</span>
+        <span class="ellipsis">...</span>
+    </div>
+    <div class="income-info">
+        <div class="icon"></div>
+        <span class="info-label">Income</span>
+        <span class="income-amount">+1024$</span>
+        <span class="ellipsis">...</span>
+    </div>
+    <span class="recent">Recent</span>
+</div>
+</body>
+</html>
