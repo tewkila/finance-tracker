26d9c3f08fbbbda27b2da06f2e034146876576b4
Tekla Berozashvili <72336776+tewkila@users.noreply.github.com>
2024-05-28 01:18:43 +0300

    update

--- settings/config.php
+++ settings/config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration settings
 $server = 'anysql.itcollege.ee'; // Database server
-$user = 'ICS0008_WT_17'; // Database username
-$database = 'ICS0008_17'; // Database name
-$password = '7a900f227b86'; // Database password
+$user = ''; // Database username
+$database = ''; // Database name
+$password = ''; // Database password
 
 // Create a connection
 $link = new mysqli($server, $user, $password, $database);
